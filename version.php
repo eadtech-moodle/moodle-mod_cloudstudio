82e2a8c905e277d90aa507504c55bd6575686de7
Eduardo Kraus <kraus@eduardokraus.com>
2024-06-27 11:09:18 -0300

    V 1.0.1

--- version.php
+++ version.php
@@ -17,16 +17,16 @@
 /**
  * version file
  *
- * @package    mod_cloudstudio
- * @copyright  2023 Eduardo kraus (http://eduardokraus.com)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   mod_cloudstudio
+ * @copyright 2024 Eduardo kraus (http://eduardokraus.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2024062500; //
+$plugin->version = 2024062503; //
 $plugin->requires = 2014051200;
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
 $plugin->component = 'mod_cloudstudio';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [];
