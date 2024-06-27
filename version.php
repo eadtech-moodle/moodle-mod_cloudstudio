ed36c6b439b5d30c5813ee8b0f05aebda9c8ca8d
Eduardo Kraus <kraus@eduardokraus.com>
2024-06-27 11:37:55 -0300

    V 1.1.2

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version = 2024062700; //
 $plugin->requires = 2014051200;
-$plugin->release = '1.0.2';
+$plugin->release = '1.1.2';
 $plugin->component = 'mod_cloudstudio';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [];
